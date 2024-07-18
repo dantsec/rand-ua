8c2c59008af7fc3e9918046b1228a10ec5864669
dantsec <cauerdsantana@gmail.com>
2024-07-17 21:04:27 -0300

    feat: add methods to generate random user agents

--- src/UserAgentGenerator.php
+++ src/UserAgentGenerator.php
@@ -4,13 +4,44 @@ namespace Dant\RandUa;
 
 class UserAgentGenerator
 {
+    protected $browsers = [
+        'chrome' => __DIR__ . '/user-agents/chrome.txt',
+        'edge' => __DIR__ . '/user-agents/edge.txt',
+        'firefox' => __DIR__ . '/user-agents/firefox.txt',
+        'opera' => __DIR__ . '/user-agents/opera.txt',
+        'safari' => __DIR__ . '/user-agents/safari.txt',
+    ];
+
     public static function create()
     {
         return new static();
     }
 
+    private function getRandomElementFromArray(array $content = []): string {
+        return $content[rand(0, count($content) - 1)];
+    }
+
     public function getUserAgent(string $browser = ''): string
     {
-        return $browser;
+        $file = file(
+            $this->browsers[$browser],
+            FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES
+        );
+        
+        return $this->getRandomElementFromArray($file);
+    }
+
+    public function getRandomUserAgent(): string
+    {
+        $avaiable_browsers = array_keys($this->browsers);
+
+        $rand_browser = $this->getRandomElementFromArray($avaiable_browsers);
+
+        $file = file(
+            $this->browsers[$rand_browser],
+            FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES
+        );
+
+        return $this->getRandomElementFromArray($file);
     }
 }
